aca45dd507072d23e850405201593978a6c99912
Toktorov <ktoktorov2005@gmail.com>
2023-09-25 18:19:11 +0600

    detail product

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from apps.settings.models import Setting
+from apps.products.models import Product
+
 # Create your views here.
+def product_detail(request, id):
+    setting = Setting.objects.latest('id')
+    product = Product.objects.get(id=id)
+    return render(request, 'products/detail.html', locals())
