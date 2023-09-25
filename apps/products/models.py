d55e3f24928f4de17fc94b9e383d3fbbdbc1a175
Toktorov <ktoktorov2005@gmail.com>
2023-09-25 13:56:39 +0600

    add model for product

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(
+        max_length=300,
+        verbose_name="Название"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True, null=True
+    )
+    price = models.CharField(
+        max_length=100,
+        verbose_name="Цена"
+    )
+    image = models.ImageField(
+        max_length=1000,
+        verbose_name="Фотография продукта"
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+
+    def __str__(self):
+        return self.title 
+    
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 from apps.settings.models import Setting
+from apps.products.models import Product
 
 # Create your views here.
 def index(request):
     setting = Setting.objects.latest('id')
+    products = Product.objects.all()
     return render(request, 'index-8.html', locals())
