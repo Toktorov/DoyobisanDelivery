68a782dbd466f0c9e5400287da39a1973ea2f660
Toktorov <ktoktorov2005@gmail.com>
2023-09-28 13:49:52 +0600

    SaleSummary model

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from rangefilter.filter import DateRangeFilter
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from django.utils.translation import gettext as _
+from django.db.models import Sum, Count
 
-
-from apps.billing.models import Billing, BillingProduct
+from apps.billing.models import Billing, BillingProduct, SaleSummary
 
 # Register your models here.
 class CustomDateFieldListFilter(admin.DateFieldListFilter):
@@ -31,6 +31,36 @@ class ProductTabularInline(admin.TabularInline):
     model = BillingProduct
     extra = 0
 
+@admin.register(SaleSummary)
+class SaleSummaryAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/sale_summary_change_list.html'
+    date_hierarchy = 'created'
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context,
+        )
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        metrics = {
+            # 'title': ,
+            'total': Count('id'),
+            'total_sales': Sum('total_price'),
+        }
+
+        response.context_data['summary'] = list(
+            qs.values('billing_products').annotate(**metrics).order_by('-created')
+        )
+        print(response)
+        print(metrics)
+
+        return response
+
 @admin.register(Billing)
 class BillingAdmin(admin.ModelAdmin):
     list_display = ('id', 'total_price', 'address', 'payment_method', 'phone', 'billing_receipt_type', 'payment_code', 'created', 'status')
@@ -38,6 +68,8 @@ class BillingAdmin(admin.ModelAdmin):
     inlines = [ProductTabularInline]
     list_filter = (('created', DateRangeFilter), ('created', CustomDateFieldListFilter),)  # Добавляем фильтр по дате
 
+
 @admin.register(BillingProduct)
 class BillingProductAdmin(admin.ModelAdmin):
     list_display = ('billing', 'product', 'quantity', 'price', 'status')
+    list_filter = (('created', DateRangeFilter), ('created', CustomDateFieldListFilter),)  # Добавляем фильтр по дате
--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from rangefilter.filter import DateRangeFilter
 
+from apps.billing.admin import CustomDateFieldListFilter
 from apps.products.models import Product
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'created')
+    search_fields = ('title', 'description', 'created')
+    list_filter = (('created', DateRangeFilter), ('created', CustomDateFieldListFilter),)  # Добавляем фильтр по дате
