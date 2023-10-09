a57dcff9c64331a8507263ad995c3c122b54eff6
Toktorov <ktoktorov2005@gmail.com>
2023-10-09 14:17:38 +0600

    order menu 40%

--- apps/tables/urls.py
+++ apps/tables/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path 
 
-from apps.tables.views import menu
+from apps.tables.views import menu, add_to_order, clear_order, remove_from_order, order
 
 
 urlpatterns = [
-    path('<int:table_uuid>/', menu, name='menu')
+    path('<int:table_uuid>/', menu, name='menu'),
+    path('order/', order, name="order"),
+    path('add_to_order/', add_to_order, name='add_to_order'),
+    path('clear_order/', clear_order, name='clear_order'),
+    path('remove_from_order/<int:id>/', remove_from_order, name='remove_from_order'),
 ]
